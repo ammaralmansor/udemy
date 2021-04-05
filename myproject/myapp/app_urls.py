dd0aecd50ca061a0feb3171fa469d7c0e01f071d
ammaralmansor <ammarend@hotmail.com>
2021-04-05 21:30:57 +0300

    after first

--- myproject/myapp/app_urls.py
+++ myproject/myapp/app_urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('',views.index,name="name")
+    path('index',views.index,name="index"),
+    path('about',views.about,name="about"),
+    path('add/<int:a>/<int:b>',views.add,name="add"),
+    path('intro/<str:name>/<int:age>',views.intro,name="intro"),
+    path('second',views.second,name="second"),
 ]
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
-
+from django.http import HttpRequest,HttpResponse,JsonResponse
 # Create your views here.
 
+
+def about(request):
+    return HttpResponse("About")
+
+def add(request,a,b):
+    return HttpResponse("Resault : {}".format(a+b))
+
+def intro(request,name,age):
+    mydict = {
+        "name" : name ,
+        "age"  : age
+    }
+    return JsonResponse(mydict)
+
 def index(request):
-    return "HelloWorld"
+    return render(request,'index.html')
+    
+def second(request):
+    return render(request,'second.html')
+
 
